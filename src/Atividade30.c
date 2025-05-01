dfb59168a702bdff81f06927b8ea228a4eeb78e3
Frinng <167120714+Frinng@users.noreply.github.com>
2025-05-01 13:53:42 -0300

    Lista01 Refeita em C

--- src/Atividade30.c
+++ src/Atividade30.c
@@ -8,21 +8,27 @@ int main(int argc, char const *argv[])
     printf("\n==========Atividade 30===========");
     printf("\n================================="); 
 
-    printf("Digite um numero: ");
+    printf("\nDigite um numero: ");
     scanf("%d", &n1);
 
-    printf("Digite um segundo numero: ");
+    printf("\nDigite um segundo numero: ");
     scanf("%d", &n2);
 
-    printf("Digite um terceiro numero: ");
+    printf("\nDigite um terceiro numero: ");
     scanf("%d", &n3);
 
     if (n1 > n2 || n1 > n3){
+
         printf("%d e o maior numero.", n1);
+
     }else if (n2 > n1 || n2 > n3){
+
         printf("%d e o maior numero.", n2);
+
     }else{
+
         printf("%d e o maior numero.", n3);
+        
     }
     
 }
