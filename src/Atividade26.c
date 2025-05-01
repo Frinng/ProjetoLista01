43ae1cb20da90b3bf51303020990bba5a2383401
Frinng <167120714+Frinng@users.noreply.github.com>
2025-05-01 13:29:20 -0300

    Lista01 Refeita em C

---
+++ src/Atividade26.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n1,n2,n3;
+    
+    printf("\n================================="); 
+    printf("\n==========Atividade 26===========");
+    printf("\n================================="); 
+
+
+
+    return 0;
+}
