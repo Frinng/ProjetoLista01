a4190937c3b7bd9baca807fc8dc143f3a1c0fb75
Frinng <167120714+Frinng@users.noreply.github.com>
2025-05-01 13:55:30 -0300

    Lista01 Refeita em C

--- src/Atividade30.c
+++ src/Atividade30.c
@@ -17,18 +17,18 @@ int main(int argc, char const *argv[])
     printf("\nDigite um terceiro numero: ");
     scanf("%d", &n3);
 
-    if (n1 > n2 || n1 > n3){
+    if (n1 > n2 && n1 > n3){
 
         printf("%d e o maior numero.", n1);
 
-    }else if (n2 > n1 || n2 > n3){
+    }else if (n2 > n1 && n2 > n3){
 
         printf("%d e o maior numero.", n2);
 
     }else{
 
         printf("%d e o maior numero.", n3);
-        
+
     }
     
 }
